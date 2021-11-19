85736cd9c1df325e786d4ad64e65353763a6fa87
aarthy4f <aarthy4f@gmail.com>
2021-11-20 00:13:03 +0530

    Removed the unwanted files

--- cypress/integration/testScenario/firstscenario.spec.js
+++
@@ -1,9 +0,0 @@
-describe('Verify Browser Stack Home Page', () => {
-
-    it('Verify Browserstack logo is visible', () => {
-
-        cy.visit('https://react-shopping-cart-67954.firebaseapp.com/');
-
-
-    })
-})
